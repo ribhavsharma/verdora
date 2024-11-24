574db619e0a95fc4a5fde6701b5cdc21ea4578e3
ribhavsharma <ribhavsharma2003@gmail.com>
2024-11-24 02:21:43 -0800

    make navbar sticky

--- client/src/components/ui/navbar.tsx
+++ client/src/components/ui/navbar.tsx
@@ -47,7 +47,7 @@ const Navbar = () => {
   );
 
   return (
-    <nav className="bg-[#226f54] shadow-md">
+    <nav className="bg-[#226f54] shadow-md sticky top-0">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
