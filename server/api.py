044b1cc3a057be0a17e1e75d0b1610084082b2a0
ribhavsharma <ribhavsharma2003@gmail.com>
2024-11-23 16:20:16 -0800

    api /image

--- server/api.py
+++ server/api.py
@@ -1,10 +1,30 @@
-from typing import Union
+from fastapi import FastAPI, Body # type: ignore
+from fastapi.middleware.cors import CORSMiddleware # type: ignore
+from pydantic import BaseModel
 
-from fastapi import FastAPI # type: ignore
+
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class ImageRequest(BaseModel):
+    data: str  # Expect the `data` field as a string
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/image")
+def image(request: ImageRequest):
+    data = request.data 
+    return {"message": "Image received successfully", "data_echo": data}
