93f8d275f67d2fd3bd5793a8839873434d94de85
Murad Shahmammadli <64356412+MuradSh@users.noreply.github.com>
2024-11-23 16:15:49 -0800

    auth-backend

--- server/api.py
+++ server/api.py
@@ -1,10 +1,46 @@
 from typing import Union
-
+from mysql_manager import MysqlManager
 from fastapi import FastAPI # type: ignore
+from pydantic import BaseModel
+import hashlib
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+mysql =MysqlManager()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace "*" with specific origins like ["http://localhost:3000"] for security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class SignupRequest(BaseModel):
+    username: str
+    password: str
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/signUp")
+def signUp(request: SignupRequest):
+
+    hashed_password = hashlib.md5(request.password.encode()).hexdigest()
+
+    mysql.insert_data(
+        "users",
+        {"username": request.username,"password": hashed_password}
+    )
+    return {"message": "User signed up successfully", "username": request.username}
+
+@app.post("/signIn")
+def signUp(request: SignupRequest):
+
+    hashed_password = hashlib.md5(request.password.encode()).hexdigest()
+
+    signInDb = mysql.select_data(
+        "users",
+        "password",
+        where_clause=f"username = '{request.username}' AND password = '{hashed_password}'"
+    )
+
+    return len(signInDb)
