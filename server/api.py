99701538932d591d086d5863fcd6864c9ba34e33
aamirahmed2004 <syedaamir099@gmail.com>
2024-11-24 14:59:23 -0800

    Added sample images and code for demonstration

--- server/api.py
+++ server/api.py
@@ -165,5 +165,5 @@ async def perform_inference(request: ImageRequest):
 
     results = classify_base64(request.image)
     ewaste_flag = flag_ewaste_base64(request.image)
-    # Return the response object
-    return {"annotations": results, "ewaste_flag": (True, 0.79)}
+
+    return {"annotations": results, "ewaste_flag": ewaste_flag}
